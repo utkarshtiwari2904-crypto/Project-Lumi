6d1e65dd7c3fd89a39bd27d98e6e67c5aedac800
utkarshtiwari2904-crypto <utkarshtiwari2904@gmail.com>
2026-07-02 22:35:51 +0530

    Add initial story structure to content.js

---
+++ content.js
@@ -0,0 +1,80 @@
+const story = [
+
+{
+chapter:"",
+title:"",
+text:"",
+button:""
+},
+
+{
+chapter:"PROLOGUE",
+title:"Every great story has a first page.",
+text:"",
+button:"Continue"
+},
+
+{
+chapter:"",
+title:"",
+text:"Mine began at a dissection table.",
+button:"Continue"
+},
+
+{
+chapter:"CHAPTER I",
+title:"The Beginning",
+text:"I met you first at the DH table. At that moment, I had absolutely no idea that one day you would become the person who would completely change the way I looked at life.",
+button:"Continue"
+},
+
+{
+chapter:"",
+title:"",
+text:"It wasn't love at first sight. It was much quieter than that.",
+button:"Continue"
+},
+
+{
+chapter:"",
+title:"First...",
+text:"I cared.",
+button:"Continue"
+},
+
+{
+chapter:"",
+title:"Then...",
+text:"I found myself noticing you more than anyone else.",
+button:"Continue"
+},
+
+{
+chapter:"",
+title:"Slowly...",
+text:"That care became attachment.",
+button:"Continue"
+},
+
+{
+chapter:"",
+title:"",
+text:"Then came something I could never explain... I worried about you. Your happiness mattered to me more than my own.",
+button:"Continue"
+},
+
+{
+chapter:"",
+title:"",
+text:"I tried ignoring it. I ignored the care. Then the attraction. Then the attachment. Even the endless worry.",
+button:"Continue"
+},
+
+{
+chapter:"",
+title:"",
+text:"But one day I realized... if all these feelings have a name... then perhaps that name is Love.",
+button:"Continue"
+},
+
+{
